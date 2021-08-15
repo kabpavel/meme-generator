7163c46ae9703f1f26ae0e33dffafba620e7aacc
pavel <kab.pavel@gmail.com>
2021-08-15 08:01:01 +0300

    added first release 9

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -4,20 +4,11 @@ function init() {
     initGallery()
     initMeme()
     hideElement('.memo-container')
-    
+    hideElement('.ref-about')
+
     addMouseListeners()
     addTouchListeners()
 
     addKeydownEventListener()
     addSubmitEventListenerToForm('.search-form')
-
-    hideElement('.cntrl-btn.btn-align-left')//todo
-    hideElement('.cntrl-btn.btn-center-text')//todo
-    hideElement('.cntrl-btn.btn-align-right')//todo
-    hideElement('.chose-font')//todo
-    hideElement('.cntrl-btn.btn-text-stroke')//todo
-    hideElement('.cntrl-btn.btn-paint')//todo
-    hideElement('.btn-share.share')//todo
-    hideElement('.btn-share.download')//todo
-    hideElement('.ref-about')//todo
 }
